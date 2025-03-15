8947723bd99c689341d006be6d74f1f8bda5038e
tai.nguyen <tai.nguyen24816@gmail.com>
2025-03-15 12:00:49 +0700

    draft can_stm32_send function

--- can_stm32.c
+++ can_stm32.c
@@ -64,13 +64,74 @@ void can_stm32_send(CAN_TypeDef * can, can_frame * frame)
     uint8_t tx_index_mailbox = 0;
 
     /* check if mailbox is empty */
-    for(tx_index_mailbox = 0; tx_index_mailbox < 3; tx_index_mailbox ++)
+    for(tx_index_mailbox = 0; tx_index_mailbox < 2; tx_index_mailbox ++)
     {
         /* in case tx mailbox empty */
         if(can->sTxMailBox[tx_index_mailbox].TIR & CAN_TI0R_TXRQ == 0U)
         {
             /* fullfill id */
-            can->sTxMailBox[tx_index_mailbox].TIR = (frame->id) << CAN_TI0R_EXID_Pos;
+            can->sTxMailBox[tx_index_mailbox].TIR |= (frame->id) << CAN_TI0R_EXID_Pos;
+
+            /* select standard id or extend id */
+            /* default configure as extended id */
+            can->sTxMailBox[tx_index_mailbox].TIR = CAN_TI0R_IDE;
+
+            /* remote frame or data frame */
+            can->sTxMailBox[tx_index_mailbox].TIR &= ~CAN_TI0R_RTR;
+
+            /* configure data len */
+            /* bxCAN only support 8 byte data */
+            can->sTxMailBox[tx_index_mailbox].TDTR = frame->dlc;
+
+            /* write data to TX mail box */
+
+            /* write 4 low byte */
+            can->sTxMailBox[tx_index_mailbox].TDLR = (frame->data[0] | frame->data[1] << 8 | frame->data[2] << 16 | frame->data[3] << 24);
+
+            /* write 4 high byte */
+            can->sTxMailBox[tx_index_mailbox].TDHR = (frame->data[4] | frame->data[5] << 8 | frame->data[6] << 16 | frame->data[7] << 24);
+
+            /* request send */
+            can->sTxMailBox[tx_index_mailbox].TIR |= CAN_TI0R_TXRQ;
+            /* polling waiting for message already sent */
+            while(can->sTxMailBox[tx_index_mailbox].TIR & CAN_TI0R_TXRQ != 0U){};
+
+            break;
+ 
+        }
+    }
+}
+
+void can_stm32_recv(CAN_TypeDef * can, can_frame * recv_frame)
+{
+    uint8_t rx_index_mailbox = 0;
+    uint8_t sub_index = 0;
+
+    /* polling waiting for message come */
+    while(can->IER & CAN_IER_FMPIE0 == 0U || \
+          can->IER & CAN_IER_FMPIE1 == 0U){};
+
+    for(rx_index_mailbox =  0; rx_index_mailbox < 3; rx_index_mailbox ++)
+    {
+        /* get id */
+        recv_frame->id = can->sFIFOMailBox[rx_index_mailbox].RIR >> CAN_RI0R_EXID_Pos;
+
+        /* get fram type */
+        recv_frame->flags = can->sFIFOMailBox[rx_index_mailbox].RIR & CAN_RI0R_RTR;
+
+        /* get data len */
+        recv_frame->flags = can->sFIFOMailBox[rx_index_mailbox].RDTR & CAN_RDT0R_DLC;
+
+        /* get 4 low byte */
+        for(sub_index = 0; sub_index < 4; sub_index ++)
+        {
+            recv_frame->data[sub_index] = (can->sFIFOMailBox[rx_index_mailbox].RDLR >> (sub_index * 8)) & 0xFF;
+        }
+
+        /* get 4 high byte */
+        for(sub_index = 0; sub_index < 4; sub_index ++)
+        {
+            recv_frame->data[sub_index + 4] = (can->sFIFOMailBox[rx_index_mailbox].RDHR >> (sub_index * 8)) & 0xFF;
         }
     }
 }
